ae4555b8bc65c79062f225561fbdde36c33d3244
Kaelin Laundry <wasabifan@outlook.com>
2021-02-08 00:20:54 -0800

    Update cpmment on sv2v hack

--- rom_tb.sv
+++ rom_tb.sv
@@ -11,7 +11,9 @@ module rom_tb();
     wire [31:0] r_data;
 
     rom #(
-        // TODO: transpile testbenches with sv2v to avoid this 
+        // TODO: the testbench is transpiled without the main sources included,
+        // so sv2v doesn't introduce this automatically. Should consider
+        // including all sources in each individual testbench.
         ._sv2v_width_init_file(14*8),
         .init_file("rom_tb_mem.hex")
     ) u_rom (
