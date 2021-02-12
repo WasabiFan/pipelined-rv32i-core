4deecfaca6077556e0cb103fca30338d19943e34
Kaelin Laundry <wasabifan@outlook.com>
2021-02-12 00:26:40 -0800

    Fix stack start address

--- firmware/start.s
+++ firmware/start.s
@@ -8,7 +8,7 @@ v2: .word   0       # halt port
 .text
 
 _start:
-    li      sp, (0x00020000 + (1024*8) - 16)
+    li      sp, (0x00020000 + (1024*4) - 16)
     lw      x1, v1
     call    main
     call    halt
