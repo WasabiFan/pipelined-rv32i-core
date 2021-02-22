b4a10826fad570d6d9ffe1ee8bcc849bbf85478a
Kaelin Laundry <wasabifan@outlook.com>
2021-02-22 13:44:12 -0800

    Make regfile synthesizable as BRAM
    The register file is now two separate RAM banks. Writes are performed on
    both, and reads for each source register happen on its own dedicated
    half independently. This simulates a dual-read-port RAM.

--- register_file.sv
+++ register_file.sv
@@ -1,3 +1,40 @@
+// one register is x0
+parameter num_regs = 32;
+
+module register_file_half(
+    input logic clock,
+    input logic reset,
+
+    input rv_reg_t rs,
+    input reg_write_control_t write_control,
+
+    output logic [XLEN-1:0] val
+);
+    logic [XLEN-1:0] xregs [0:num_regs-1];
+
+    reg_write_control_t last_write_control;
+    logic [XLEN-1:0] last_read_val;
+    rv_reg_t last_rs;
+
+    always_ff @(posedge clock) begin
+        if (write_control.enable)
+            xregs[write_control.which_register] <= write_control.value;
+
+        last_write_control <= write_control;
+        last_read_val <= xregs[rs];
+        last_rs <= rs;
+    end
+
+    always_comb begin
+        if (last_rs == 0)
+            val = 0;
+        else if (last_write_control.enable && last_write_control.which_register == last_rs)
+            val = last_write_control.value;
+        else
+            val = last_read_val;
+    end
+endmodule
+
 module register_file(
     input logic clock,
     input logic reset,
@@ -9,50 +46,34 @@ module register_file(
     output logic [XLEN-1:0] rs1_val,
     output logic [XLEN-1:0] rs2_val
 );
-    // one register is x0
-    parameter num_regs = 32;
-
-    logic [XLEN-1:0] xregs [0:num_regs-1];
 
     // for debugging in sim
     `ifdef SIMULATION
     logic [XLEN-1:0] reg_x01_ra, reg_x02_sp, reg_x08_s0, reg_x10_a0, reg_x11_a1, reg_x15_a5, reg_x19_s3, reg_x21_s5, reg_x23_s7;
-    assign reg_x01_ra = xregs[1];
-    assign reg_x02_sp = xregs[2];
-    assign reg_x08_s0 = xregs[8];
-    assign reg_x10_a0 = xregs[10];
-    assign reg_x11_a1 = xregs[11];
-    assign reg_x15_a5 = xregs[15];
-    assign reg_x19_s3 = xregs[19];
-    assign reg_x21_s5 = xregs[21];
-    assign reg_x23_s7 = xregs[23];
+    assign reg_x01_ra = rs1_half.xregs[1];
+    assign reg_x02_sp = rs1_half.xregs[2];
+    assign reg_x08_s0 = rs1_half.xregs[8];
+    assign reg_x10_a0 = rs1_half.xregs[10];
+    assign reg_x11_a1 = rs1_half.xregs[11];
+    assign reg_x15_a5 = rs1_half.xregs[15];
+    assign reg_x19_s3 = rs1_half.xregs[19];
+    assign reg_x21_s5 = rs1_half.xregs[21];
+    assign reg_x23_s7 = rs1_half.xregs[23];
     `endif
 
-    always_ff @(posedge clock) begin
-        if (reset) begin
-            xregs[0] <= 0;
-            `ifndef SIMULATION
-            // Not required, but for reproducibility...
-            xregs <= '{default:0};
-            `endif
-        end else begin
-            if (write_control.enable && write_control.which_register != 0) begin // avoid writing to x0
-                xregs <= xregs;
-                xregs[write_control.which_register] <= write_control.value;
-            end else begin
-                xregs <= xregs;
-            end
+    register_file_half rs1_half (
+        .clock            (clock),
+        .reset            (reset),
+        .rs               (rs1),
+        .write_control    (write_control),
+        .val              (rs1_val)
+    );
 
-            if (write_control.enable && write_control.which_register != 0 && write_control.which_register == rs1)
-                rs1_val <= write_control.value;
-            else
-                rs1_val <= xregs[rs1];
-
-            if (write_control.enable && write_control.which_register != 0 && write_control.which_register == rs2)
-                rs2_val <= write_control.value;
-            else
-                rs2_val <= xregs[rs2];
-        end
-    end
-
+    register_file_half rs2_half (
+        .clock            (clock),
+        .reset            (reset),
+        .rs               (rs2),
+        .write_control    (write_control),
+        .val              (rs2_val)
+    );
 endmodule;
